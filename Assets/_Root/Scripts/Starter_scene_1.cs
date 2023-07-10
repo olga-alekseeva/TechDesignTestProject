58f5086d4a0516215f3971c488486f0872cba6a4
olga-alekseeva <oliasca@gmail.com>
2023-07-10 13:58:45 +0300

    Localization
    Добавила локализацию для кнопок

--- Assets/_Root/Scripts/Starter_scene_1.cs
+++ Assets/_Root/Scripts/Starter_scene_1.cs
@@ -7,10 +7,14 @@ public class Starter_scene_1 : MonoBehaviour
     [SerializeField] public Button nextSceneButton;
     [SerializeField] public GameObject nextSceneButtonObject;
     [SerializeField] public Animator[] managingAnimator;
+    [SerializeField] public Button exitButton;
+
     void Start()
     {
         nextSceneButtonObject.SetActive(false);
         nextSceneButton.onClick.AddListener(SwitchScene);
+        exitButton.onClick.AddListener(ExitGame);
+
         int i = 0;
         for (i = 0; i < managingAnimator.Length; i++)
         {
@@ -21,6 +25,10 @@ public class Starter_scene_1 : MonoBehaviour
     public void SwitchScene()
     {
         SceneManager.LoadScene(1);
+    }
+    private void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
--- Assets/_Root/Scripts/Starter_scene_2.cs
+++ Assets/_Root/Scripts/Starter_scene_2.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,10 +8,17 @@ public class Starter_scene_2 : MonoBehaviour
 
     [SerializeField] public Button swithSceneButton;
     [SerializeField] public GameObject swithSceneButtonObject;
+    [SerializeField] public Button exitButton;
     void Start()
     {
         swithSceneButtonObject.SetActive(false);
         swithSceneButton.onClick.AddListener(SwitchScene);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
     }
 
     public void SwitchScene()
